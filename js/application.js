e126fd21572b32e09019cb63299d3101dda581f6
Yana <yanatsulya@gmail.com>
2014-02-18 00:48:29 +0200

    init commit

---
+++ js/application.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Yana on 17.02.14.
+ */
+jQuery(function($){
+
+    showPhoto();
+
+    function showPhoto() {
+        $('#dress').on('click', function () {
+            $("#show").dialog({
+                width: 250,
+                height: 500,
+                resizable: false,
+                draggable: false,
+                modal: true
+            });
+        });
+    }
+});
